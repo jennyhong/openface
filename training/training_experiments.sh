5edb071de7e4bdda5cc91280ea92648c1650face
Jenny Hong <jenny.yhong@gmail.com>
2016-03-07 20:11:42 -0800

    training script: add cuda and cudnn flags

--- training/training_experiments.sh
+++ training/training_experiments.sh
@@ -16,8 +16,8 @@ rm -r $FINAL_WORK_DIR*
 for ((  i = 1 ;  i <= 1;  i++  ))
 do
   if $USE_GPU ; then
-    qlua main.lua -data $DATA_DIR -modelDef $MODEL_DIR/fitnets_firsthalf$i.def.lua -trainFirstHalf -cache $WORK_DIR-$i  -nEpochs $NEPOCHS;
-    qlua main.lua -data $DATA_DIR -modelDef $MODEL_DIR/fitnets_all$i.def.lua -trainFullStudent -cache $FINAL_WORK_DIR-$i  -firstHalfCached $WORK_DIR-$i/1/model_$NEPOCHS.t7;
+    qlua main.lua -data $DATA_DIR -modelDef $MODEL_DIR/fitnets_firsthalf$i.def.lua -cuda -cudnn -trainFirstHalf -cache $WORK_DIR-$i  -nEpochs $NEPOCHS;
+    qlua main.lua -data $DATA_DIR -modelDef $MODEL_DIR/fitnets_all$i.def.lua -cuda -cudnn -trainFullStudent -cache $FINAL_WORK_DIR-$i  -firstHalfCached $WORK_DIR-$i/1/model_$NEPOCHS.t7;
   else
     qlua main.lua -data $DATA_DIR -modelDef $MODEL_DIR/fitnets_firsthalf$i.def.lua -trainFirstHalf -cache $WORK_DIR-$i  -nEpochs $NEPOCHS -peoplePerBatch 5 -imagesPerPerson 5 -epochSize 5;
     qlua main.lua -data $DATA_DIR -modelDef $MODEL_DIR/fitnets_all$i.def.lua -trainFullStudent -cache $FINAL_WORK_DIR-$i  -firstHalfCached $WORK_DIR-$i/1/model_$NEPOCHS.t7 -nEpochs $NEPOCHS -peoplePerBatch 5 -imagesPerPerson 5 -epochSize 5;
